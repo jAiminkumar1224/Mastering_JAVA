df1a06a65e86bbedb793ad4cde7759c504bf47d6
Jaiminkumar Patel <cap.jemspatel1212@gmail.com>
2025-04-12 00:15:52 +0530

    Loops

---
+++ loops.java
@@ -0,0 +1,29 @@
+package Mastering_JAVA;
+
+// We can, We should & We will
+// I can, I should & I will
+
+public class loops {
+
+    public static void main(String[] args){
+        
+//for loop        
+        // for(int i=0;i<5;i++){
+        //     System.out.println(i);
+        // }
+//while loop
+        // int i=0;
+        // while(i<5){
+        //     System.out.println("*");
+        //     i++;
+        // }    
+//do while loop
+        int i=0;
+        do{
+            System.out.println("*");
+            i++;
+        }while(i<5);
+ 
+        
+    }
+}
